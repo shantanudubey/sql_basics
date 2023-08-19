f9b5269664ce652a56a8452864d24024c67903a8
Shantanu Dubey <shantanu.dubey1@outlook.com>
2023-08-19 22:24:00 +0530

    adding exercises during class

---
+++ exercise_01.sql
@@ -0,0 +1,39 @@
+# Exercises
+
+create DATABASE ats;
+use ats;
+
+create table techmap(
+applicant_id int not NULL,
+tech varchar(25) not null
+);
+desc techmap;
+
+insert into techmap values
+(1, "ds"), 
+(1, "py"),
+(1, "sql"),
+(2, "sql"),
+(2, "py"),
+(2, "ds"),
+(2, "tableau"),
+(3, "tableau"),
+(3, "sql"),
+(1, "tableau"),
+(1, "c"),
+(1, "c++"),
+(3, "as3"),
+(3, "java"),
+(2, "php"),
+(2, "java");
+
+select applicant_id, tech from techmap ORDER BY applicant_id;
+
+SELECT applicant_id from techmap where tech in ("ds", "sql", "py");
+
+# Using Nested Queries to get the applicants who know ds, sql and py
+select applicant_id from techmap where tech = "ds" and applicant_id in (
+select applicant_id from techmap where tech = "sql" and applicant_id in (
+select applicant_id from techmap where tech = "java"
+));
+
